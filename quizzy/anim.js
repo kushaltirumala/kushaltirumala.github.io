4a2fc2d2589f34fbd473ecd133959006a63b8178
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-16 12:45:47 -0700

    implemented quiz interface

---
+++ quizzy/anim.js
@@ -0,0 +1,10 @@
+$(function(){
+	$("#intro").animate({
+        height: '10%',
+        width: '50%'}, "slow");
+
+	$("#button").animate({
+		    left: '75%',
+    		bottom: '165px'
+	}, "slow");
+})
---
+++ quizzy/app.js
@@ -0,0 +1,36 @@
+$(function(){
+
+$("#userInfoButton").click(function(){
+	submitUserInfo();
+});
+
+	function submitUserInfo() {
+		var username = $("#username").val();
+		var pass = $("#password").val();
+		if(username==null || pass == null){
+			alert("Please enter full Quizlet credentials, otherwise we cannot make a set for you :(");
+		} else {
+			quizletAuth(username, pass);
+		}
+	}
+
+
+	function quizletAuth(username, pass) {
+		var str = makeid();
+		var redirectURI = "https://quizlet.com/authorize?response_type=code&client_id=4msU8P4c2B&scope=write_set&state="+str;
+		//alert(redirectURI);
+		window.open(redirectURI,'auth time');
+	}
+
+	function makeid()
+		{
+		    var text = "";
+		    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		    for( var i=0; i < 5; i++ )
+		        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+		    return text;
+		}
+
+});

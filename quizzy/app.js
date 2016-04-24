442fed710c2b6760d8914c957ad1f1e33c35e8da
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-24 14:57:02 -0700

    corsanywhere pt3

--- quizzy/app.js
+++ quizzy/app.js
@@ -1,5 +1,13 @@
 $(function(){
 
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
 var textFill = "";
 $("#recognizeButton").click(function(){
 		alert('pressed');

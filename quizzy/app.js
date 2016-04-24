21f85936c683a27d83319d51e09b699dd943f40c
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-24 15:05:48 -0700

    pt4

--- quizzy/app.js
+++ quizzy/app.js
@@ -3,8 +3,8 @@ $(function(){
 $.ajaxPrefilter( function (options) {
   if (options.crossDomain && jQuery.support.cors) {
     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    //options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    options.url = "http://cors.corsproxy.io/url=" + options.url;
   }
 });
 

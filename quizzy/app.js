bc940b717ca2136ca32c23ee816867681ff80f49
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-23 21:28:41 -0700

    cors anywhere?

--- quizzy/app.js
+++ quizzy/app.js
@@ -164,7 +164,7 @@ function showProgress(p) {
 		var code = currentURL.substring(currentURL.indexOf("code=")+5);
 
 		//alert('about to post req');
-		var url ="https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/index.html";
+		var url ="https://cors-anywhere.herokuapp.com/https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/index.html";
 		 $.ajax({
             type:"POST",
             beforeSend: function (request)

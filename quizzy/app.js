c71ae1b8e2ab526bac96e83e223fc6cc4f79c5de
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-24 16:21:04 -0700

    ok no prefilters

--- quizzy/app.js
+++ quizzy/app.js
@@ -1,11 +1,11 @@
 $(function(){
 
-$.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-  }
-});
+// $.ajaxPrefilter( function (options) {
+//   if (options.crossDomain && jQuery.support.cors) {
+//     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+//     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+//   }
+// });
 
 var textFill = "";
 $("#recognizeButton").click(function(){
